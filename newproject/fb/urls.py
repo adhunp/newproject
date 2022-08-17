3f8b664df5002897387766d8950c5a8b45545ba9
adhunp <adhunp8@gmail.com>
2022-08-17 11:33:37 +0530

    first push

---
+++ newproject/fb/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .import views
+
+urlpatterns=[
+    path('facebook',views.fb),
+    path('validation',views.domvalidation),
+    path('val',views.val),
+    path('calculater',views.calculater),
+     path('array',views.array),
+
+]
---
+++ newproject/fb/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def fb(request):
+    return render (request,'fb.html')
+
+def domvalidation(request):
+    return render (request,'domvalidation.html')
+
+def val(request):
+    return render (request,'val.html')
+
+def calculater(request):
+    return render (request,'calculater.html')
+
+def array(request):
+    return render (request,'domarray.html')
+
+
+
